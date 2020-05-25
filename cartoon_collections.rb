1003bb0df50aff0b78d41ef0dd33c14af95df680
Michael Jones <djyoungjones@gmail.com>
2020-05-25 22:46:05 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -11,14 +11,16 @@ def square_array(array)
 end
 
 def summon_captain_planet(planeteer_calls)
-
+ planeteer_calls = []
+  array.collect do |calls|
+    planeteer_calls << "#{calls.capitalize}!"
+  end
+  planeteer_calls
 end
 
 
 def long_planeteer_calls(planeteer_calls)
-  array.any? do |word|
-    word.length > 4
-end
+  
 end
 
 def find_valid_calls(planeteer_calls)
